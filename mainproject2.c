2e86bbe88f50e5bd7ddc6802a53885c4738cfdfb
Emmarcdp <149601264+Emmarcdp@users.noreply.github.com>
2023-11-08 23:46:40 +0100

    Create mainproject2.c

---
+++ mainproject2.c
@@ -0,0 +1,17 @@
+// Étape 1 : Lire le graphe à partir du fichier binaire
+LoadGraphFromBinaryFile("graph.bin", &Graph);
+
+// Étape 2 : Mettre en place l'algorithme A*
+AStarPath PathData[GraphOrder];
+
+// Étape 3 : Exécuter l'algorithme A*
+unsigned node_start = FindNodeByName(Graph, 'A');
+unsigned node_goal = FindNodeByName(Graph, 'B');
+bool result = AStar(Graph, PathData, GraphOrder, node_start, node_goal);
+
+if (result) {
+    // Étape 4 : Afficher le chemin résultant
+    PrintOptimalPath(Graph, PathData, node_start, node_goal);
+} else {
+    printf("Pas de chemin trouvé.\n");
+}
